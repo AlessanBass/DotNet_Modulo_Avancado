ed54234697cb9b5445e32efe205a72d8767b2baf
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-03-12 15:04:57 -0300

    Exercicio 1

---
+++ Tarefas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Tarefas{
+
+    class Lampada{
+        bool ligada;
+        public Lampada(bool _ligada){
+            this.ligada = _ligada;
+        }
+
+        public void ligar(){
+            this.ligada = true;
+        }
+
+        public void desligar(){
+            this.ligada = false;
+        }
+
+        public void imprimir(){
+            if(this.ligada){
+                Console.WriteLine("A lampada está ligada");
+            }else{
+                Console.WriteLine("A lampada está desligada");
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Lampada lamp = new Lampada(true);
+            lamp.imprimir();
+            lamp.desligar();
+            lamp.imprimir();
+        }
+    }
+
+
+
+}
